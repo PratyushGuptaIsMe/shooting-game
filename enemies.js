d4c05ecb764043198206e080f8ba9929072914f2
PratyushGuptaIsMe <pratyushngupta@gmail.com>
2025-08-17 18:38:37 -0500

    added 2 semicolons (╹ڡ╹ )

--- enemies.js
+++ enemies.js
@@ -74,10 +74,10 @@ class Enemies{
             this.y = -35;
         }
         if((this.hitbox.x) + (this.hitbox.w) > this.game.canvasWidth){
-            this.x = this.game.canvasWidth - (this.spriteWidth * 2 - 150) - 70
+            this.x = this.game.canvasWidth - (this.spriteWidth * 2 - 150) - 70;
         }
         if(this.hitbox.y + this.hitbox.h > this.game.canvasHeight){
-            this.y = this.game.canvasHeight - (this.spriteHeight * 2 - 35) - 35
+            this.y = this.game.canvasHeight - (this.spriteHeight * 2 - 35) - 35;
         }
 
     }
